3f7ee799663483ac59d88acd0196533d289e7cd6
bruno-amil@hotmail.com <bruno-amil@hotmail.com>
2020-12-06 10:29:45 -0300

    alteracoes no package.json

---
+++ build/index.js
@@ -0,0 +1,11 @@
+import * as CSS from './css';
+import * as JS from './js';
+import * as JSON from './json';
+import * as SCSS from './scss';
+
+module.exports = {
+    CSS,
+    JS,
+    JSON,
+    SCSS
+}
---
+++ build/js/_variables.js
@@ -0,0 +1,10 @@
+/**
+ * Do not edit directly
+ * Generated on Sun, 06 Dec 2020 13:01:36 GMT
+ */
+
+export const ColorBrandPrimaryDarkest = "#003351";
+export const ColorBrandPrimaryDark = "#004980";
+export const ColorBrandPrimaryBase = "#0066b2";
+export const ColorBrandPrimaryLight = "#e5f4ff";
+export const ColorBrandPrimaryLighttest = "#e5f4ff";
